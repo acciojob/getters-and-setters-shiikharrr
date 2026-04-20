c407b95cd60e25ceedbd1918afc5b8cccabc9329
Shikhar Gupta <69209995+shiikharrr@users.noreply.github.com>
2026-04-20 12:31:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name, age) {
+		this._name = name;
+		this._age= age;
+	}
+	get name() {
+		return this._name;
+	}
 
-class Student extends Person {}
+	set name(name) {
+		this._name = name;
+	}
 
-class Teacher extends Person {}
+	get age() {
+		return this._age;
+	}
+
+	set age(age) {
+		this._age =age;
+	}
+}
+
+
+
+class Student extends Person {
+	study() {
+		console.log(`${this.name) is studying`);
+	}
+}
+
+class Teacher extends Person {
+	teach() {
+		console.log(`$(this.nmae) is teaching `)
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
